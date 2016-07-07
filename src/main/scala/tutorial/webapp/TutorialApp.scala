4a68a23d6e7b41b70ebaf1bc0e6fbb059d98be48
Joern Bernhardt <jb@campudus.com>
2016-07-07 20:11:36 +0200

    Testing types
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- src/main/scala/tutorial/webapp/TutorialApp.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -2,8 +2,9 @@ package tutorial.webapp
 
 import org.scalajs.dom
 
+import scala.scalajs.js
 import scala.scalajs.js.JSApp
-import scala.scalajs.js.annotation.JSExport
+import scala.scalajs.js.annotation.{JSExport, JSExportAll}
 
 @JSExport
 object TutorialApp extends JSApp {
@@ -12,15 +13,24 @@ object TutorialApp extends JSApp {
   def main(): Unit = {
     dom.document.getElementById("hello").innerHTML = "test"
     println("Hello world!")
-    val blubb = new scala.scalajs.js.Object {
-      val int: Int = 123
-      val bool: Boolean = true
-
-      def testMethod(): Int = 456
-    }
+    val outerInt = 456
+    val blubb = new Something(789)
     val result = blubb.testMethod()
-    dom.console.log("test", blubb)
+    val method = () => blubb.testMethod()
     dom.console.log("blubb", result)
+    dom.console.log("int", blubb.int)
+    dom.console.log("boolean", blubb.bool)
+    dom.console.log("test", blubb.test)
+    dom.console.log("method", method())
   }
 
+}
+
+@JSExportAll
+class Something(outerInt: Int) {
+  val int: Int = 123
+  val bool: Boolean = true
+  val test: Int = outerInt
+
+  def testMethod(): Int = 789 + test
 }
