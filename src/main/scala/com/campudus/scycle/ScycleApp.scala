a727f4d4f2ba31dde1e406a31ec7fde0128e6fde
Joern Bernhardt <jb@campudus.com>
2016-08-31 07:57:50 +0200

    Add a lot of debugging to see how to properly connect request / response Rx
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- src/main/scala/com/campudus/scycle/ScycleApp.scala
+++ src/main/scala/com/campudus/scycle/ScycleApp.scala
@@ -22,30 +22,54 @@ object ScycleApp extends JSApp {
 
   def logic(sources: collection.Map[String, Driver])(implicit ctx: Ctx.Owner): Rx[collection.Map[String, _]] = {
     val domDriver = sources("dom").asInstanceOf[DomDriver]
+    val httpDriver = sources("http").asInstanceOf[HttpDriver]
     val buttonClicks1 = domDriver.selectEvents(".get-first", "click")
     val buttonClicks2 = domDriver.selectEvents(".abort-load", "click")
+    println("getting response")
+    val response = httpDriver.response()
+    println(s"got response as Rx[String]")
+
+    response.trigger {
+      println("there was a new response in the logic")
+    }
 
     val request = Rx {
+      println("evaluating request")
       if (buttonClicks1() != null) {
         println("set request!")
-        Some(Get("http://jsonplaceholder.typicode.com/users/1"))
+        Get("http://jsonplaceholder.typicode.com/users/1")
       } else {
         println("no request to set!")
-        None
+        null
       }
     }
+
+    val user = Rx {
+      println("evaluating user")
+      val userResponse = response()
+      println(s"the user response is $userResponse")
+      val userToReturn = if (userResponse != null) {
+        val user = userResponse.split(" ")
+        User(user(0), user(1), user(2))
+      } else {
+        User("(name)", "(email)", "(website)")
+      }
+      println(s"user to return = $userToReturn")
+      userToReturn
+    }
+    println("got a user")
 
     Rx {
       Map(
         // Logic (functional)
         "dom" -> {
           Div(children = Seq(
-            Button(className = "get-first", children = Seq(Text(if (request().isEmpty) "Get first user" else "Getting first user"))),
-            Button(className = "abort-load", children = Seq(Text(if (request().isEmpty) "Do nothing..." else "Stop loading"))),
+            Button(className = "get-first", children = Seq(Text(if (request() == null) "Get first user" else "Getting first user"))),
+            Button(className = "abort-load", children = Seq(Text(if (request() == null) "Do nothing..." else "Stop loading"))),
             Div(className = "user-details", children = Seq(
-              H1(className = "user-name", children = Seq(Text("(name)"))),
-              Div(className = "user-email", children = Seq(Text("(email)"))),
-              A(className = "user-website", href = "https://example.com", children = Seq(Text("(website)")))
+              H1(className = "user-name", children = Seq(Text(user().name))),
+              Div(className = "user-email", children = Seq(Text(user().email))),
+              A(className = "user-website", href = user().website, children = Seq(Text(user().website)))
             ))
           ))
         },
@@ -65,7 +89,9 @@ object ScycleApp extends JSApp {
     logicOut =>
       println("hello, HTTP driver!")
 
-      new HttpDriver(logicOut.asInstanceOf[Rx[Option[Request]]])(ctx)
+      new HttpDriver(logicOut.asInstanceOf[Rx[Request]])(ctx)
   }
 
 }
+
+case class User(name: String, email: String, website: String)
--- src/main/scala/com/campudus/scycle/http/HttpDriver.scala
+++ src/main/scala/com/campudus/scycle/http/HttpDriver.scala
@@ -3,20 +3,23 @@ package com.campudus.scycle.http
 import com.campudus.scycle.Driver
 import rx.{Ctx, Rx, Var}
 
-class HttpDriver(input: Rx[Option[Request]])(implicit ctx: Ctx.Owner) extends Driver {
+import scala.util.Random
+
+class HttpDriver(input: Rx[Request])(implicit ctx: Ctx.Owner) extends Driver {
 
   println("initializing http driver...")
-
-  Rx {
-    input.triggerLater {
-      val request = input()
-      println(s"new request received: $request")
-    }
+  val request = Rx {
+    val r = Var[Option[Request]](Option(input()))
+    println(s"got a new request ${r()}")
+    r()
+  }
+  val resp = Rx {
+    println("evaluating response")
+    val receivedRequest = request()
+    println(s"the request received: $receivedRequest")
+    println(s"received new request ${receivedRequest.getOrElse("none")}")
+    s"(name_${Random.nextInt()}) (email_${Random.nextInt()}) (website_${Random.nextInt()})"
   }
 
-  def responses()(implicit ctx: Ctx.Owner): Rx[Response] = {
-    val responseEvent = Var[Response](null)
-
-    responseEvent
-  }
+  def response()(implicit ctx: Ctx.Owner): Rx[String] = resp
 }
