677df9edcad03186bb08a779dffe80248306a279
Joern Bernhardt <jb@campudus.com>
2016-10-09 00:48:16 +0200

    Simplify test and remove debug statements
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- src/test/scala/com/campudus/scycle/ScycleSuite.scala
+++ src/test/scala/com/campudus/scycle/ScycleSuite.scala
@@ -16,34 +16,12 @@ class ScycleSuite extends AsyncFunSpec {
     it("can read data from a logic") {
       val inputText = "Hello World!"
       val p = Promise[String]
-      Scycle.run({ drivers =>
-        Map(
-          "text" -> {
-            println("test main???")
-            val input$ = Observable.just(inputText)
-            println(s"observable.main=${input$}")
-            val mapped = input$.map({ input =>
-              println(s"mapped main-input=$input")
-              input
-            })
-            mapped
-          }
-        )
-      },
-        Map(
-          "text" -> { (sth$: Observable[_]) =>
-            println("test driver???")
-            println(s"observable.driver=${sth$}")
-            val mapped = sth$.map({ text =>
-              println(s"mapped driver-input=$text")
-              p.success(text.asInstanceOf[String])
-            })
-            mapped
-          }
-        )
-      )
+      Scycle.run(drivers =>
+        Map("test" -> Observable.just(inputText)),
+        Map("test" -> { (sth$: Observable[_]) => sth$.map(text => p.success(text.asInstanceOf[String])) }))
       p.future.map(text => assert(text === inputText))
     }
+
   }
 
 }
