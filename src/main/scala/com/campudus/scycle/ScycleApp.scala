d734cae5af6a519a83ef94b0967a949a75485ff4
Joern Bernhardt <jb@campudus.com>
2016-12-08 00:40:19 +0100

    Modify simple app to use an observable from driver - does not work yet
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- src/main/scala/com/campudus/scycle/ScycleApp.scala
+++ src/main/scala/com/campudus/scycle/ScycleApp.scala
@@ -23,9 +23,16 @@ object ScycleApp extends JSApp {
 
   def logic(drivers: Sources): Sinks = {
     println(s"ScycleApp.logic($drivers)")
+    val domDriver$ = drivers("dom").asInstanceOf[Subject[DomDriver]]
+    val clicks$ = domDriver$.flatMap(driver => driver.selectEvent("#app", "click"))
+    var counter = 0
+
     Map(
       "dom" -> {
-        Observable.just(Div(id = "app", children = Seq(Text("hello from scycle"))))
+        Observable.just(null)/*.concat(clicks$)*/.map(_ => {
+          counter += 1
+          Div(id = "app", children = Seq(Text(s"hello from scycle - clicks=$counter")))
+        })
       }
     )
   }
