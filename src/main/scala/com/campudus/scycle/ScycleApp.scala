c8624018a7b9ea184d66b4ffb5405c35356e5d05
Joern Bernhardt <jb@campudus.com>
2016-12-18 22:13:05 +0100

    Remove domEvents$ stream
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- src/main/scala/com/campudus/scycle/ScycleApp.scala
+++ src/main/scala/com/campudus/scycle/ScycleApp.scala
@@ -24,23 +24,18 @@ object ScycleApp extends JSApp {
 
   def logic(sources: Sources): Sinks = {
     println(s"ScycleApp.logic($sources)")
-    val domEvents$ = sources("dom").asInstanceOf[Subject[Event]]
-    println(s"ScycleApp.logic($sources):domEvents$$ = ${domEvents$}")
     val clicks$ = drivers("dom").asInstanceOf[DomDriver].selectEvent("#app", "click")
     println(s"ScycleApp.logic:feed domEvents$$ into clicks$$")
-    clicks$.subscribe(domEvents$)
     var counter = 0
 
     Map(
       "dom" -> {
         println("ScycleApp.return:before domEvents$")
-        val cObs = clicks$.map(ev => {
+        clicks$.startWith(null).map(_ => {
           println(s"ScycleApp.return:in domEvents$$:$$ev")
           counter += 1
           Div(id = "app", children = Seq(Text(s"hello from scycle - clicks=$counter")))
         })
-        cObs.subscribe(_ => println("don't care about cObs ?"))
-        cObs
       }
     )
   }
