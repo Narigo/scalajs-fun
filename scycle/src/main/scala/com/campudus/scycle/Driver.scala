105f72a4de927d8bb26cc7af733cd5c99f108ac0
Joern Bernhardt <jb@campudus.com>
2017-04-08 15:06:01 +0200

    Add debug to driver - cannot see any further
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- scycle/src/main/scala/com/campudus/scycle/Driver.scala
+++ scycle/src/main/scala/com/campudus/scycle/Driver.scala
@@ -5,7 +5,12 @@ import rxscalajs.{Observable, Subject}
 
 trait Driver[A] {
 
-  def createSubject(): Subject[A] = Subject()
+  def createSubject(): Subject[A] = {
+    org.scalajs.dom.console.log("in Driver.createSubject()")
+    val s = Subject.apply[A]()
+    org.scalajs.dom.console.log("after Driver.createSubject:Subject.apply()")
+    s
+  }
 
   def subscribe(inputs: Observable[A]): AnonymousSubscription = inputs.subscribe(_ => null)
 
